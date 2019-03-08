57e1fad705fe3491db20fba58ac0f60c0c228714
Fábio <fsmaiorano@gmail.com>
2019-03-08 10:30:52 -0300

    remove docs

--- docs/js/chunk-2d22502a.9226b0d7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22502a"],{e328:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("p",[n._v("organizations")])},u=[],c={},l=c,o=t("2877"),r=Object(o["a"])(l,a,u,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d22502a.9226b0d7.js.map
